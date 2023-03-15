48329c15d46ed16a76246ad326ab23d56903e8ac
Pedro Calado <pdcalado@gmail.com>
2023-03-15 19:00:51 +0000

    Trim 'v' prefix from version in welcome block.

--- src/blocks/welcome.tsx
+++ src/blocks/welcome.tsx
@@ -3,7 +3,7 @@ import TitleSubtitleComponent from "./titleSubtitle";
 const WelcomeComponent = ({ title }: { title: string }) =>
   TitleSubtitleComponent({
     title,
-    subtitle: "v" + process.env.REACT_APP_VERSION!,
+    subtitle: "v" + process.env.REACT_APP_VERSION!.replace(/^v/, ""),
     object: false,
   });
 
